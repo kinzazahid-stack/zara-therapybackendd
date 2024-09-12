193356e0baa90ea563821c765cc1a7a310f1efd7
kinzazahid-stack <kinzaz548@gmail.com>
2024-09-12 16:55:05 +0500

    Database Connected

---
+++ db.js
@@ -0,0 +1,4 @@
+const mongoose = require ('mongoose')
+
+mongoose.connect('mongodb+srv://kinzaz548:tXgl09ghreBB2r0b@webcluster.vgx6y.mongodb.net/zara-therapy')
+  .then(() => console.log('Connected!'));
